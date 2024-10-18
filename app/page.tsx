a7177130ca62f39a3353e8053caccbaff11bedb7
Trevor Elkins <trevor@emergetools.com>
2024-10-17 23:43:17 -0400

    Center content

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,18 @@ export default function Home() {
       <div className="absolute w-[640px] h-[232px] bg-black/20 transform translate-x-8 -translate-y-8"></div>
 
       {/* Main content */}
-      <div className="relative w-[640px] h-[232px] p-8 border border-gray-700/20 bg-[rgba(3,3,3,1)] backdrop-blur-2xl backdrop-saturate-200 backdrop-opacity-80 dark:border-neutral-800 dark:bg-zinc-800/30 flex flex-col justify-center">
-        <h1 className="text-3xl font-bold text-[rgba(253,253,253,1)] dark:text-gray-100 mb-4">
-          Highstorm Software
-        </h1>
-        <h2 className="text-xl font-medium text-[rgba(253,253,253,1)] dark:text-gray-100 mb-4">
-          Crafting memorable native iOS and Android apps
-        </h2>
-        <p className="text-[rgba(253,253,253,1)] dark:text-gray-100">
-          Check back soon for our latest projects and updates.
-        </p>
+      <div className="relative w-[640px] h-[232px] p-8 border border-gray-700/20 bg-[rgba(3,3,3,1)] backdrop-blur-2xl backdrop-saturate-200 backdrop-opacity-80 dark:border-neutral-800 dark:bg-zinc-800/30 flex flex-col justify-center items-center">
+        <div className="w-full max-w-[500px]">
+          <h1 className="text-3xl font-bold text-[rgba(253,253,253,1)] dark:text-gray-100 mb-4">
+            Highstorm Software
+          </h1>
+          <h2 className="text-xl font-medium text-[rgba(253,253,253,1)] dark:text-gray-100 mb-4">
+            Crafting memorable native iOS and Android apps
+          </h2>
+          <p className="text-[rgba(253,253,253,1)] dark:text-gray-100">
+            Check back soon for our latest projects and updates.
+          </p>
+        </div>
       </div>
     </div>
   )
