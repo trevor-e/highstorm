ff588cd27eae2e1751520721ee41882474e7842c
Trevor Elkins <trevor@emergetools.com>
2024-10-17 23:53:27 -0400

    Fix page layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,27 +16,29 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${recursive.className} bg-slate-950`}>
-        <div className="relative h-screen">
-          <div className="absolute inset-0 bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url("/highstorm-background.png")' }}>
-            <nav className="absolute top-0 left-0 right-0 bg-[rgba(3,3,3,1)] backdrop-blur-sm shadow-md">
-              <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <ul className="flex justify-center space-x-4 py-4">
-                  <li className="font-cardo p-2 font-semibold text-gray-200 hover:bg-slate-800 rounded-lg transition duration-250 ease-in-out">
-                    <Link href="/">Home</Link>
-                  </li>
-                  <li className="font-cardo p-2 font-semibold text-gray-200 hover:bg-slate-800 rounded-lg transition duration-250 ease-in-out">
-                    <Link href="mailto:hello@highstorm.dev">Contact</Link>
-                  </li>
-                </ul>
-              </div>
-            </nav>
-            <main className="flex items-center justify-center h-full w-full pt-16">
-              {children}
-            </main>
+    <html lang="en" className="h-full">
+      <body className={`${recursive.className} bg-slate-950 flex flex-col h-full overflow-hidden`}>
+        <nav className="bg-[rgba(3,3,3,0.8)] backdrop-blur-sm shadow-md z-10 flex-shrink-0">
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <ul className="flex justify-center space-x-4 py-4">
+              <li className="font-cardo p-2 font-semibold text-gray-200 hover:bg-slate-800 rounded-lg transition duration-250 ease-in-out">
+                <Link href="/">Home</Link>
+              </li>
+              <li className="font-cardo p-2 font-semibold text-gray-200 hover:bg-slate-800 rounded-lg transition duration-250 ease-in-out">
+                <Link href="mailto:hello@highstorm.dev">Contact</Link>
+              </li>
+            </ul>
           </div>
-        </div>
+        </nav>
+        <main className="flex-grow relative">
+          <div
+            className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+            style={{ backgroundImage: 'url("/highstorm-background.png")' }}
+          />
+          <div className="relative z-10 h-full">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
   );
